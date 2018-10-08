2d326ab8192849f3269782b0ec6ca4149ef93b86
Kamron Hays <kamron.hays@live.com>
2018-10-07 22:05:28 -0400

    Refactor account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,6 +49,18 @@ class User < ApplicationRecord
     BCrypt::Password.new(digest).is_password?(token)
   end
 
+  # Activates an account.
+  def activate
+    # Updates two attributes with with a single call, which hits the database
+    # only once.
+    update_columns(activated: true, activated_at: Time.zone.now)
+  end
+
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   private
 
     # Converts email to all lower-case.
